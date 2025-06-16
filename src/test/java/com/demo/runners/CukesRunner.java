cdd15e1573025d2361e2fd3ab0ad12e218e8e3e7
kiymet <akgozkiymet@gmail.com>
2025-06-16 12:42:41 -0500

    initial commit

---
+++ src/test/java/com/demo/runners/CukesRunner.java
@@ -0,0 +1,17 @@
+package com.demo.runners;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"html:target/cucumber.html"},
+features = "src/test/resources/features",
+        glue = "com/demo/step_definitions",
+        tags = "",
+        dryRun = false
+)
+
+
+public class CukesRunner {
+}
+
