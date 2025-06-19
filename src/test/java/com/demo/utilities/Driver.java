b14fad47569f7103080dfc654520291ea78f8dbd
ersu-amine <ersu.amine@gmail.com>
2025-06-19 13:37:26 -0400

    Update driver with get and close method

--- src/test/java/com/demo/utilities/Driver.java
+++ src/test/java/com/demo/utilities/Driver.java
@@ -1,10 +1,35 @@
 package com.demo.utilities;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Driver {
-    private Driver(){
+    private Driver() {
 
     }
+
     private static WebDriver driver;
+
+    public static WebDriver getDriver() {
+        String browser = ConfigurationReader.getProperty("browser");
+
+        if (driver == null) {
+            if (browser.equals("chrome")) {
+                driver = new ChromeDriver();
+                driver.manage().window().fullscreen();
+
+            } else if (browser.equals("firefox")) {
+                driver = new FirefoxDriver();
+                driver.manage().window().fullscreen();
+            }
+        }
+        return driver;
+    }
+
+    public static void closeDriver(){
+        if(driver!=null){
+            driver.quit();
+        }
+    }
 }
