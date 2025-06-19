e47c43c841fbe2af90c26fbbaa51c9357286ab1f
kiymet <akgozkiymet@gmail.com>
2025-06-19 12:10:43 -0500

    second commit

--- src/test/java/com/demo/step_definitions/Hooks.java
+++ src/test/java/com/demo/step_definitions/Hooks.java
@@ -1,4 +1,10 @@
 package com.demo.step_definitions;
 
+import com.demo.utilities.BrowserUtils;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
 public class Hooks {
+
+
 }
--- src/test/java/com/demo/utilities/BrowserUtils.java
+++ src/test/java/com/demo/utilities/BrowserUtils.java
@@ -1,4 +1,12 @@
 package com.demo.utilities;
 
 public class BrowserUtils {
+    public static void wait(int seconds) {
+        try {
+            Thread.sleep(seconds * 1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
