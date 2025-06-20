b932add03c574969b8276895677f42ce3d7025bc
ersu-amine <ersu.amine@gmail.com>
2025-06-20 00:38:47 -0400

    update home, login pages

--- src/test/java/com/demo/pages/LoginPage.java
+++ src/test/java/com/demo/pages/LoginPage.java
@@ -1,7 +1,25 @@
 package com.demo.pages;
 
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class LoginPage extends BasePage {
 
-    private
+    @FindBy(css = "input[data-qa='signup-name']")
+    private WebElement signupName;
+
+    @FindBy(css = "input[data-qa='signup-email']")
+    private WebElement signupEmail;
+
+    @FindBy(css = "button[data-qa='signup-button']")
+    private WebElement signupButton;
+
+    @FindBy(xpath = "//h2[text()='New User Signup!']")
+    private WebElement signupHeader;
+
+    public void verifySignupHeader(){
+        Assert.assertTrue("New user signup header not displayed", signupHeader.isDisplayed());
+    }
 
 }
