3c53fe499491b1bc4e1946ab59a02fa50e9cad56
ersu-amine <ersu.amine@gmail.com>
2025-06-24 01:54:19 -0400

    add steps implementation

--- src/test/java/com/demo/pages/LoginPage.java
+++ src/test/java/com/demo/pages/LoginPage.java
@@ -1,11 +1,19 @@
 package com.demo.pages;
 
+import com.demo.utilities.BrowserUtils;
+import com.demo.utilities.ConfigurationReader;
+import com.github.javafaker.Faker;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.Assert;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class LoginPage extends BasePage {
+    private static final Logger LOG = LogManager.getLogger();
+    Faker faker = new Faker();
 
+    //login and initial signup page
     @FindBy(css = "input[data-qa='signup-name']")
     private WebElement signupName;
 
@@ -20,6 +28,20 @@ public class LoginPage extends BasePage {
 
     public void verifySignupHeader(){
         Assert.assertTrue("New user signup header not displayed", signupHeader.isDisplayed());
+    }
+
+    public void inputNameEmail(){
+        String dateTime = BrowserUtils.getTimeStamp();
+        String name = faker.name().firstName();
+        String email = ConfigurationReader.getProperty("email_base") + "+" + dateTime + "@gmail.com";
+        signupName.sendKeys(name);
+        LOG.info("signup name: {}", name);
+        signupEmail.sendKeys(email);
+        LOG.info("email: {}", email);
+    }
+
+    public void clickSignUpButton(){
+        signupButton.click();
     }
 
 }
